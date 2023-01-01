01a517a734b293fb3674c5afca6254d8e79cf3c1
Ruben Luciano <soloruben@gmailOC.com>
2023-01-01 19:24:46 -0400

    New features

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace MyConsoleApp
+﻿using System;
+namespace MyConsoleApp
 {
     internal class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, This is my World!");
+            
+        string? name;
+            Console.WriteLine("Escribe tu Edad:");
+            int.TryParse(Console.ReadLine(),out int edad);
+             Console.WriteLine("Escribe tu nombre:");
+            name=Console.ReadLine();
+        
+            Console.WriteLine("Hello my name  is: "+name+ ", my age is :"+edad.ToString()+ ", This is my World!");
         }
     }
 }
